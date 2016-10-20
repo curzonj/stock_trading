3d199bb5b7fe1f55a66325224542ef165e5a5abb
Jordan Curzon <curzonj@gmail.com>
2016-10-20 02:59:25 -0700

    securities as parameters

--- strat_research_v1/monthly_rebalance.py
+++ strat_research_v1/monthly_rebalance.py
@@ -3,14 +3,17 @@ from zipline.api import *
 def universe():
     return [ "SPY", "LQD" ]
 
-def initialize(context):
+def initialize(context, **kwargs):
+    context.symbols = [ symbol(a) for a in kwargs.pop('securities')]
+    context.weights = [ float(a) for a in kwargs.pop('weights').split(',') ]
+
     schedule_function(rebalance,
                       date_rule=date_rules.month_start(),
                       time_rule=time_rules.market_open())
 
 def rebalance(context, data):
-    order_target_percent(symbol("SPY"), 0.6)
-    order_target_percent(symbol("LQD"), 0.4)
+    for idx, symbol in enumerate(context.symbols):
+        order_target_percent(symbol, context.weights[idx])
 
 def handle_data(context, data):
     pass
